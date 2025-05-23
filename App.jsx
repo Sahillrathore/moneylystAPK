51a95a9a34d1a3bfd483ca32f9317b214d6b4ec6
Sahillrathore <sahil921031@gmail.com>
2025-05-23 13:48:14 +0530

    routes

--- App.jsx
+++ App.jsx
@@ -8,6 +8,8 @@ import Welcome from "./src/screens/Welcome";
 import { AuthProvider } from "./context/AuthContext";
 import Signup from "./src/screens/Signup";
 import Login from "./src/screens/Login";
+import AddTransaction from "./src/screens/AddTransaction";
+import Accounts from "./src/screens/Accounts";
 
 const Stack = createStackNavigator();
 
@@ -18,6 +20,8 @@ export default function App() {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="TransactionHistory" component={TransactionHistory} />
+          <Stack.Screen name="AddTransaction" component={AddTransaction} />
+          <Stack.Screen name="Accounts" component={Accounts} />
           <Stack.Screen name="Welcome" component={Welcome} />
           <Stack.Screen name="Signup" component={Signup} />
           <Stack.Screen name="Login" component={Login} />
